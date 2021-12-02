add577249edfb599a203018ec6129d844ac2e81c
ezexedge <ezeedge@gmail.com>
2021-12-01 21:00:37 -0300

    cargando rutas

--- back/server.js
+++ back/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOption = {
-  origin: 'http://localhost:8080',
+  origin: 'http://46.101.206.241:8080',
 };
 app.use(cors(corsOption));
 
